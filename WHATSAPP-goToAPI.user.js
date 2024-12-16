4dcc00040d4f07d4022ff0246f16ffa719284ea9
Ra <rarodriguezrosado@gmail.com>
2024-12-16 20:03:13 +0100

    WHATSAPP-goToWeb & WHATSAPP-goToAPI: fix non spanish numbers starting with 6 or 7

--- WHATSAPP-goToAPI.user.js
+++ WHATSAPP-goToAPI.user.js
@@ -4,19 +4,25 @@
 // @match          https://web.whatsapp.com/send*
 // @match          https://api.whatsapp.com/send*
 // @run-at         document-start
-// @version        1.0
+// @version        1.0.1
 // @updateURL      https://raw.githubusercontent.com/RaRodRos/userscripts/refs/heads/master/WHATSAPP-goToAPI.user.js
 // ==/UserScript==
 
-if (window.location.href.match(/https:\/\/web|&text=|\?phone=([67]\d{8}|.*\D.*)/)) {
+if (
+  window.location.href.match(
+    /https:\/\/web|&text=|\?phone=([67]\d{8}\D|[67]\d{8}$|.*\D.*)/
+  )
+) {
   window.location.assign(
     "https://api.whatsapp.com/send/?phone=" +
-    decodeURIComponent(window.location.href).replace(
-      /.*?\?phone=\D*(.*?.(?=[?&])|.*).*/,
-      (match, phoneParam) => {
-        const numberResult = phoneParam.replace(/\D/g, "");
-        return /^[67]\d{8}$/.test(numberResult) ? "34" + numberResult : numberResult;
-      }
-    )
+      decodeURIComponent(window.location.href).replace(
+        /.*?\?phone=\D*(.*?.(?=[?&])|.*).*/,
+        (match, phoneParam) => {
+          const numberResult = phoneParam.replace(/\D/g, "");
+          return /^[67]\d{8}$/.test(numberResult)
+            ? "34" + numberResult
+            : numberResult;
+        }
+      )
   );
 }
--- WHATSAPP-goToWeb.user.js
+++ WHATSAPP-goToWeb.user.js
@@ -4,19 +4,25 @@
 // @match          https://api.whatsapp.com/send*
 // @match          https://web.whatsapp.com/send*
 // @run-at         document-start
-// @version        1.0
+// @version        1.0.1
 // @updateURL      https://raw.githubusercontent.com/RaRodRos/userscripts/refs/heads/master/WHATSAPP-goToWeb.user.js
 // ==/UserScript==
 
-if (window.location.href.match(/https:\/\/api|&text=|\?phone=([67]\d{8}|.*\D.*)/)) {
+if (
+  window.location.href.match(
+    /https:\/\/api|&text=|\?phone=([67]\d{8}\D|[67]\d{8}$|.*\D.*)/
+  )
+) {
   window.location.assign(
     "https://web.whatsapp.com/send/?phone=" +
-    decodeURIComponent(window.location.href).replace(
-      /.*?\?phone=\D*(.*?.(?=[?&])|.*).*/,
-      (match, phoneParam) => {
-        const numberResult = phoneParam.replace(/\D/g, "");
-        return /^[67]\d{8}$/.test(numberResult) ? "34" + numberResult : numberResult;
-      }
-    )
+      decodeURIComponent(window.location.href).replace(
+        /.*?\?phone=\D*(.*?.(?=[?&])|.*).*/,
+        (match, phoneParam) => {
+          const numberResult = phoneParam.replace(/\D/g, "");
+          return /^[67]\d{8}$/.test(numberResult)
+            ? "34" + numberResult
+            : numberResult;
+        }
+      )
   );
 }
